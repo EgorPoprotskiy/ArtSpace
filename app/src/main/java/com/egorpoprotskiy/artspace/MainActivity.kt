d3cee01e3d04a3299499639c1c8c1767413e2720
Egor Poprotskiy <egorpoprotskiy@gmail.com>
2024-08-24 19:06:56 +1000

    1_create_interface

--- app/src/main/java/com/egorpoprotskiy/artspace/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/artspace/MainActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.egorpoprotskiy.artspace.ui.theme.ArtSpaceTheme
 import org.w3c.dom.Text
 
@@ -32,36 +41,85 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp() {
-    Column {
-        Text(text = stringResource(id = R.string.app_name))
+    Column(
+//        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        ImageAdd(
+            image = ImageBitmap.imageResource(R.drawable.girl_0)
+        )
+        DescriptionAndYearAdd(
+            description = stringResource(id = R.string.description_0),
+            year = stringResource(id = R.string.year_0)
+        )
+        ButtonAdd(
+            buttonPreviews = stringResource(id = R.string.button_previous),
+            buttonNext = stringResource(id = R.string.button_next))
     }
 }
 
 @Composable
-fun ImageApp(
+fun ImageAdd(
     image: ImageBitmap,
     modifier: Modifier = Modifier
 ) {
     Image(
         image,
-        contentDescription = null
+        contentDescription = null,
+        modifier = Modifier
+            .size(300.dp, 200.dp)
     )
 }
 
 @Composable
-fun DescriptionAndYearApp(
+fun DescriptionAndYearAdd(
     description: String,
-    year: String
+    year: String,
+    modifier: Modifier = Modifier
 ) {
-    Text(
-        text = description
-    )
-    Text(
-        text = year)
+    Column(
+        modifier = modifier
+            .padding(20.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = description,
+            fontStyle = FontStyle.Italic,
+        )
+        Text(
+            text = year,
+            fontStyle = FontStyle.Italic,
+        )
+    }
+}
+
+@Composable
+fun ButtonAdd(
+    buttonPreviews: String,
+    buttonNext: String,
+    modifier: Modifier = Modifier
+) {
+    Row {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(120.dp,40.dp)
+        ) {
+            Text(text = buttonPreviews)
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.size(120.dp,40.dp)
+        ) {
+            Text(text = buttonNext)
+        }
+    }
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
