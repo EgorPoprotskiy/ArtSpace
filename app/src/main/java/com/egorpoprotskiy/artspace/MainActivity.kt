a2fdd16923a0dfaf644070208aa69a3183738809
Egor Poprotskiy <egorpoprotskiy@gmail.com>
2024-08-24 14:52:19 +1000

    nothing

--- app/src/main/java/com/egorpoprotskiy/artspace/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/artspace/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.egorpoprotskiy.artspace.ui.theme.ArtSpaceTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +24,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtSpaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ArtSpaceApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ArtSpaceApp() {
+    Column {
+        Text(text = stringResource(id = R.string.app_name))
+    }
+}
+
+@Composable
+fun ImageApp(
+    image: ImageBitmap,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        image,
+        contentDescription = null
     )
 }
+
+@Composable
+fun DescriptionAndYearApp(
+    description: String,
+    year: String
+) {
+    Text(
+        text = description
+    )
+    Text(
+        text = year)
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceApp()
     }
 }
